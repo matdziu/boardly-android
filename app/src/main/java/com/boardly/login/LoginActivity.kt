422674da209b4c2109b3898ed085616efd9493df
Mateusz Dziubek <matdziu@gmail.com>
2018-06-28 20:15:42 +0200

    Added layout for sign up screen

--- app/src/main/java/com/boardly/login/LoginActivity.kt
+++ app/src/main/java/com/boardly/login/LoginActivity.kt
@@ -1,13 +1,18 @@
 package com.boardly.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.boardly.R
+import com.boardly.signup.SignUpActivity
+import kotlinx.android.synthetic.main.activity_login.createAccountButton
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        createAccountButton.setOnClickListener { startActivity(Intent(this, SignUpActivity::class.java)) }
     }
 }
