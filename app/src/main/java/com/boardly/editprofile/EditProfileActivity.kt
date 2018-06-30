b311b7e3806d59068c4c65aeff3e2a97429e338d
Mateusz Dziubek <matdziu@gmail.com>
2018-06-30 15:19:15 +0200

    Added EditProfileActivity

---
+++ app/src/main/java/com/boardly/editprofile/EditProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.boardly.editprofile
+
+import android.os.Bundle
+import com.boardly.R
+import com.boardly.base.BaseActivity
+
+class EditProfileActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.activity_edit_profile)
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/boardly/home/HomeActivity.kt
+++ app/src/main/java/com/boardly/home/HomeActivity.kt
@@ -1,13 +1,17 @@
 package com.boardly.home
 
+import android.content.Intent
 import android.os.Bundle
 import com.boardly.R
 import com.boardly.base.BaseActivity
+import com.boardly.editprofile.EditProfileActivity
 
 class HomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setContentView(R.layout.activity_home)
         super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, EditProfileActivity::class.java))
     }
 }
