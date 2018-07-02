5fe72bc3e7cb0aefea8dde92d1690b149890b3b7
Mateusz Dziubek <matdziu@gmail.com>
2018-07-02 21:39:54 +0200

    Now launching search places screen

--- app/src/main/java/com/boardly/addevent/AddEventActivity.kt
+++ app/src/main/java/com/boardly/addevent/AddEventActivity.kt
@@ -1,8 +1,11 @@
 package com.boardly.addevent
 
+import android.app.Activity
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.os.Bundle
+import android.support.annotation.StringRes
+import android.widget.Toast
 import com.boardly.R
 import com.boardly.base.BaseActivity
 import com.boardly.constants.PLACE_AUTOCOMPLETE_REQUEST_CODE
@@ -14,7 +17,9 @@ import com.google.android.gms.location.places.ui.PlaceAutocomplete
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_add_event.pickGameButton
 import kotlinx.android.synthetic.main.activity_add_event.pickPlaceButton
+import kotlinx.android.synthetic.main.activity_add_event.placeTextView
 import javax.inject.Inject
+
 
 class AddEventActivity : BaseActivity(), AddEventView {
 
@@ -45,19 +50,38 @@ class AddEventActivity : BaseActivity(), AddEventView {
         super.onStop()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    val place = PlaceAutocomplete.getPlace(this, data)
+                    placeTextView.text = place.address
+                }
+                PlaceAutocomplete.RESULT_ERROR -> showErrorToast(R.string.generic_error)
+                Activity.RESULT_CANCELED -> hideSoftKeyboard()
+            }
+        }
+    }
+
+
     override fun render(addEventViewState: AddEventViewState) {
 
     }
 
     private fun launchPlaceSearch() {
         try {
-            val placeSearchIntent = PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+            val placeSearchIntent = PlaceAutocomplete
+                    .IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
                     .build(this)
             startActivityForResult(placeSearchIntent, PLACE_AUTOCOMPLETE_REQUEST_CODE)
         } catch (e: GooglePlayServicesRepairableException) {
-
+            showErrorToast(R.string.gps_update_needed)
         } catch (e: GooglePlayServicesNotAvailableException) {
-
+            showErrorToast(R.string.gps_not_available)
         }
+    }
+
+    private fun showErrorToast(@StringRes errorTextId: Int) {
+        Toast.makeText(this, errorTextId, Toast.LENGTH_LONG).show()
     }
 }
