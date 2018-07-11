b7669dcd7c8a5f6c77e223bf7c37cad1fdf9b3df
Mateusz Dziubek <matdziu@gmail.com>
2018-07-11 15:43:42 +0200

    Added listener to seek bar for updating distance text view

--- app/src/main/java/com/boardly/filter/FilterActivity.kt
+++ app/src/main/java/com/boardly/filter/FilterActivity.kt
@@ -1,8 +1,11 @@
 package com.boardly.filter
 
 import android.os.Bundle
+import android.widget.SeekBar
 import com.boardly.R
 import com.boardly.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_filter.distanceSeekBar
+import kotlinx.android.synthetic.main.activity_filter.distanceTextView
 
 class FilterActivity : BaseActivity() {
 
@@ -10,5 +13,23 @@ class FilterActivity : BaseActivity() {
         setContentView(R.layout.activity_filter)
         super.onCreate(savedInstanceState)
         showBackToolbarArrow(true, this::finish)
+        initDistanceFilter(50)
+    }
+
+    private fun initDistanceFilter(initialProgress: Int) {
+        distanceSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                distanceTextView.text = getString(R.string.max_distance_text, progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // unused
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // unused
+            }
+        })
+        distanceSeekBar.progress = initialProgress
     }
 }
