076a1937cba4c38c8be2c3027cfdf3438d499ad1
Mateusz Dziubek <matdziu@gmail.com>
2018-07-12 16:22:15 +0200

    Added tests to Home and Filter screens

--- app/src/test/java/com/boardly/filter/FilterViewModelTest.kt
+++ app/src/test/java/com/boardly/filter/FilterViewModelTest.kt
@@ -1,10 +1,29 @@
 package com.boardly.filter
 
+import com.boardly.retrofit.gamesearch.models.Game
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
+import io.reactivex.Observable
+import org.junit.Test
 
 class FilterViewModelTest {
 
-    private val filterInteractor: FilterInteractor = mock()
+    private val testGame = Game(image = "url/to/image")
+    private val filterInteractor: FilterInteractor = mock {
+        on { it.fetchGameDetails(any()) } doReturn
+                Observable.just(PartialFilterViewState.GameDetailsFetched(testGame))
+                        .cast(PartialFilterViewState::class.java)
+    }
     private val filterViewModel = FilterViewModel(filterInteractor)
     private val filterViewRobot = FilterViewRobot(filterViewModel)
+
+    @Test
+    fun testSuccessfulGameDetailsFetching() {
+        filterViewRobot.emitGameId("testGameId")
+
+        filterViewRobot.assertViewStates(
+                FilterViewState(),
+                FilterViewState(gameImageUrl = testGame.image))
+    }
 }
--- app/src/test/java/com/boardly/home/HomeViewModelTest.kt
+++ app/src/test/java/com/boardly/home/HomeViewModelTest.kt
@@ -1,10 +1,32 @@
 package com.boardly.home
 
+import com.boardly.filter.models.Filter
+import com.boardly.home.models.Event
+import com.boardly.home.models.UserLocation
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
+import io.reactivex.Observable
+import org.junit.Test
 
 class HomeViewModelTest {
 
-    private val homeInteractor: HomeInteractor = mock()
+    private val testEventList = listOf(Event("1", "TestEvent", "testGameId"))
+    private val homeInteractor: HomeInteractor = mock {
+        on { it.fetchEvents(any(), any(), any()) } doReturn
+                Observable.just(PartialHomeViewState.EventsFetchedState(testEventList))
+                        .cast(PartialHomeViewState::class.java)
+    }
     private val homeViewModel = HomeViewModel(homeInteractor)
     private val homeViewRobot = HomeViewRobot(homeViewModel)
+
+    @Test
+    fun testSuccessfulEventFetching() {
+        homeViewRobot.emitFilteredFetchTrigger(UserLocation(1.0, 2.0), Filter())
+
+        homeViewRobot.assertViewStates(
+                HomeViewState(),
+                HomeViewState(progress = true),
+                HomeViewState(eventList = testEventList))
+    }
 }
