a32da1c697a69e5e2a8ba6e31a2fdd4a65ba9060
Mateusz Dziubek <matdziu@gmail.com>
2018-08-09 16:04:03 +0200

    Refactored EditProfileInteractor

---
+++ app/src/main/java/com/boardly/editprofile/network/EditProfileService.kt
@@ -0,0 +1,4 @@
+package com.boardly.editprofile.network
+
+interface EditProfileService {
+}
---
+++ app/src/main/java/com/boardly/editprofile/network/EditProfileServiceImpl.kt
@@ -0,0 +1,6 @@
+package com.boardly.editprofile.network
+
+import com.boardly.base.BaseServiceImpl
+
+class EditProfileServiceImpl : EditProfileService, BaseServiceImpl() {
+}
