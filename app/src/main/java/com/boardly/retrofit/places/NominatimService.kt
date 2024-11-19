5a69c2431eab41da195f68525f6c3e6c94caf9b9
Mateusz Dziubek <>
2024-11-19 07:52:58 +0100

    Make nominatim service work

--- app/src/main/java/com/boardly/retrofit/places/NominatimService.kt
+++ app/src/main/java/com/boardly/retrofit/places/NominatimService.kt
@@ -3,10 +3,12 @@ package com.boardly.retrofit.places
 import com.boardly.retrofit.places.models.PlaceSearchResult
 import io.reactivex.Observable
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface NominatimService {
 
+    @Headers("User-Agent: Boardly-Android")
     @GET("search?format=json")
     fun search(@Query("q") query: String): Observable<List<PlaceSearchResult>>
 }
